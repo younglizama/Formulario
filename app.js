005a679bf0ae21d48e686b7e2acc0fd752b05637
younglizama <128271686+younglizama@users.noreply.github.com>
2023-04-15 17:24:50 -0400

    Update app.js

--- app.js
+++ app.js
@@ -12,24 +12,6 @@ var comentario = document.getElementById('comentario');
 var error = document.getElementById('error');
 error.style.color = 'red';
 
-//function enviarformulario(){
-//    console.log('enviando formulario...');
-
-//    var mensajeserror = [];
-
- //   if (nombre.value === null || nombre.value ===''){
- //       mensajeserror.push('engresa tu nombre');
- //   }
- //   if (password.value === null || password.value ===''){
- //       mensajeserror.push('engresa tu password');
- //   }
-
- //   error.innerHTML = mensajeserror.join(', ' );
-
-
- //   return false;
-//}
-
 var form = document.getElementById(formulario);
     form.addEventListener("submit", function(evt){
         evt.preventDefault();
